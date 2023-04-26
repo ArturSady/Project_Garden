523762ca3f3422beb2874575e17e8e1f835bdb27
Artur Sady <asady502@gmail.com>
2023-04-26 18:25:11 +0200

    add classes: Tree, Dog, Flower, Bench, add constructor and methods in Tree class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Tree tree = new Tree();
+        System.out.println(tree);
     }
 }
---
+++ src/main/java/org/example/Tree.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Tree {
+    Random random = new Random();
+    ArrayList<String> trees = new ArrayList();
+    private String tree1 = "Buk";
+    private String tree2 = "Brzoza";
+    private String tree3 = "Sosna";
+    private String tree4 = "Jodła";
+    private String tree5 = "Klon";
+    private String tree6 = "Dąb";
+    private String randomTree;
+    private int amountFruit;
+public void init(){
+    trees.add(tree1);
+    trees.add(tree2);
+    trees.add(tree3);
+    trees.add(tree4);
+    trees.add(tree5);
+    trees.add(tree6);
+}
+public int getRandomAmountFruit(){
+    return random.nextInt(100) * 10;
+}
+
+    public Tree() {
+        amountFruit = getRandomAmountFruit();
+        randomTree = getRandomTree();
+    }
+
+    public String getRandomTree(){
+    init();
+    int index = random.nextInt(trees.size());
+    return trees.get(index);
+
+}
+
+    @Override
+    public String toString() {
+        return "Tree{" +
+                "randomTree='" + randomTree + '\'' +
+                ", amountFruit=" + amountFruit +
+                '}';
+    }
+}
+
